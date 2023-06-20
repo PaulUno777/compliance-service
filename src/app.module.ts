ae587d15a8552e373da7d7925a7b87d659a438b5
PaulUno777 <78661160+PaulUno777@users.noreply.github.com>
2023-06-20 11:55:44 +0100

    IAT sanctions migration features

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { MigrationService } from './migration/migration.service';
+import { MigrationModule } from './migration/migration.module';
+import { HelperModule } from './helpers/helper.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MigrationModule,
+    HelperModule,
+  ],
+  providers: [MigrationService,],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  const configService = app.get(ConfigService);
+  //Open API Documentation
+  const config = new DocumentBuilder()
+    .setTitle('KAMIX Compliance Service')
+    .setDescription('Compliance Rest API Docs')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  //Cross-origin Configurations
+  app.enableCors();
+
+  //start server
+  const PORT = configService.get('PORT') || 3000;
+  await app.listen(PORT);
+  console.log(`Server listening on port: ${PORT}`);
 }
 bootstrap();
